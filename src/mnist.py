3fb1d08c0bef84b4db568d63990b5f4a5a02d7f3
Qinyao He <hqy@megvii.com>
2016-01-19 16:56:32 +0800

    mnist 99.4%

--- src/mnist.py
+++ src/mnist.py
@@ -10,19 +10,29 @@ def mnist():
     sklearn.utils.shuffle(train_x, train_y, random_state=0)
     sklearn.utils.shuffle(test_x, test_y, random_state=0)
 
-    model = Model(0.01, 0.9, 0.0005, 100, 100)
+    model = Model(0.01, 0.9, 0.0005, 64, 200)
     model.add_layer(layers.ReshapeLayer(1, 28, 28))
+
     model.add_layer(layers.ConvolutionLayer((3, 3), 8, 1, 0, layers.rectify))
-    model.add_layer(layers.PoolingLayer((2, 2)))  # 13 * 13 * 8
-    model.add_layer(layers.ConvolutionLayer((2, 2), 16, 8, 0, layers.rectify))
-    model.add_layer(layers.PoolingLayer((2, 2)))  # 6 * 6 * 16
-    model.add_layer(layers.ConvolutionLayer((3, 3), 32, 16, 0, layers.rectify))
-    model.add_layer(layers.PoolingLayer((2, 2)))  # 2 * 2 * 32
+    model.add_layer(layers.ConvolutionLayer((3, 3), 8, 8, 0, layers.rectify))
+    model.add_layer(layers.PoolingLayer((2, 2)))  # 12 * 12 * 8
+
+    model.add_layer(layers.ConvolutionLayer((3, 3), 64, 8, 0, layers.rectify))
+    model.add_layer(layers.ConvolutionLayer((3, 3), 64, 64, 0, layers.rectify))
+    model.add_layer(layers.PoolingLayer((2, 2)))  # 4 * 4 * 64
+
+    model.add_layer(layers.ConvolutionLayer((2, 2), 128, 64, 0, layers.rectify))
+    model.add_layer(layers.ConvolutionLayer((2, 2), 128, 128, 0, layers.rectify))
+    model.add_layer(layers.DropoutLayer(0.5))
+    model.add_layer(layers.PoolingLayer((2, 2)))  # 1 * 1 * 128
+
     model.add_layer(layers.FullConnectedLayer(128, 128, 0, layers.rectify))
     model.add_layer(layers.DropoutLayer(0.5))
     model.add_layer(layers.FullConnectedLayer(128, 10))
+
     model.add_layer(layers.SoftmaxLayer())
     model.set_loss_function(layers.CrossEntropyLoss)
+
     model.build()
     print 'build model complete'
     model.train_model(train_x, train_y, test_x, test_y)
